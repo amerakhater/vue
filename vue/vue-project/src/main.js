78f6ed61fd68a72b6836fd1adf716afbec9a2951
Amera Khater <ameraahmed1815@gmail.com>
2025-08-29 15:43:37 +0300

    upload

---
+++ vue/vue-project/src/main.js
@@ -0,0 +1,10 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+// import navBar from './components/navBar.vue'
+
+
+const app = createApp(App)
+// app.component("navbarComponent", navBar)
+app.mount('#app')
